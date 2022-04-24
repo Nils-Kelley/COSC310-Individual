80bbebed6756e9202cb6ddc563f5d08570b131d2
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-04-23 21:10:13 -0700

    Add files via upload

--- GUI1.java
+++ GUI1.java
@@ -1,134 +1,139 @@
 package main;
 
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-class GUI1 extends JFrame{
 
-    private JTextArea area = new JTextArea(); //Chat Area
-   private JScrollPane scroll;
-    private JTextField field = new JTextField();//Chat Field
-    private JButton button = new JButton();// The Button
-    private JLabel label = new JLabel();// A label for the button (i.e., "return")
+class GUI1 extends JFrame {
 
-    Boolean ex = false;
-    int excount = 0;
+	private JTextArea area = new JTextArea(); // Chat Area
+	private JScrollPane scroll;
+	private JTextField field = new JTextField();// Chat Field
+	private JButton button = new JButton();// The Button
+	private JLabel label = new JLabel();// A label for the button (i.e., "return")
 
-   public GUI1(){
+	Boolean ex = false;
+	int excount = 0;
 
-       //adds scrollable feature
-       scroll = new JScrollPane();
-       scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-       scroll.setSize(500,420);
-       scroll.getViewport().setBackground(Color.black);
-       scroll.getViewport().add(area);
+	public GUI1() {
 
+		// Adding the feature enabling to scroll
+		scroll = new JScrollPane();
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.setSize(660, 420);
+		scroll.getViewport().setBackground(Color.black);
+		scroll.getViewport().add(area);
 
-        Survey survey = new Survey();
-        Device device = new Device();//creates instance of device
-        solution sol = new solution(device);
+		Survey survey = new Survey();
+		Device device = new Device();// creates instance of device
+		solution sol = new solution(device);
 
-        JFrame frame = new JFrame();
-        frame.setSize(500,500); //SCREEN SIZE
-        frame.setResizable(false);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.getContentPane();
+		JFrame frame = new JFrame();
+		frame.setSize(675, 500); // SCREEN SIZE
+		frame.setResizable(false);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.getContentPane();
 
-        frame.setTitle("Team 30 - A COSC310 Virtual Assistant"); //Title
+		frame.setTitle("Nils' Final Project - A COSC310 Virtual Assistant"); // Title
+		// Adding different parts to frame
+		// frame.add(area);
+		frame.add(scroll);
+		frame.add(field);
+		frame.add(button);
 
-        //Adding different parts to frame
-        //frame.add(area);
-       frame.add(scroll);
-        frame.add(field);
-        frame.add(button);
+		// AREA SETTINGS
+		area.setSize(675, 420); // SIZE
+		area.setLocation(0, 0); // LOCATION
+		area.setBackground(Color.white); // BACKGROUND COLOR
+		area.setForeground(Color.BLUE); // TEXT COLOR
+		area.setAutoscrolls(true);
 
+		// field settings
+		field.setSize(576, 20);
+		field.setLocation(1, 420);
 
+		// BUTTON SETTINGS
+		button.setSize(675, 60); // SIZE
+		button.setLocation(575, 420); // LOCATION
 
-//AREA SETTINGS
-        area.setSize(500,420); //SIZE
-        area.setLocation(0,0); //LOCATION
-        area.setBackground(Color.black); //BACKGROUND COLOR
-        area.setForeground(Color.WHITE); //TEXT COLOR
-        area.setAutoscrolls(true);
+		// LABEL SETTING
+		label.setText("RETURN"); // TEXT
+		label.setAlignmentY(button.getAlignmentY() - 2);
+		button.add(label); // Assigning the Label to the Bottom
 
+		// opening statement
+		area.append(
+				"-*---*---*---*---*---*---*---*---*---*----*---*--*---*---*---*---*---*---*---*---*---*----*---*--*---*---*---*---*---*---*---*---*---*----*---*----*----*----*---*---*----*---*----*----*-- \n");
+		area.append(
+				"                                             Hello! I am Nils Kelley's COSC310 Final Project! \n                            "
+						+ "                 I am a modification of Team 30's Virtual Product Assistant!! \n"
+						+ "-*---*---*---*---*---*---*---*---*---*----*---*--*---*---*---*---*---*---*---*---*---*----*---*--*---*---*---*---*---*---*---*---*---*----*---*----*----*----*---*---*----*---*----*----*-- \n"
+						+ "                                            (-)    TO EXIT PRROGRAM: Type 'exit' or 'done'\n"
+						+ "                                            (-)    TO SEE KEYWORDS: Type 'keywords' \n"
+						+ "                                            (-)    FOR DEFINITION OF KEYWORDS: Type 'define' before the KEYWORD \n"
+						+ "                                            (-)    TO TRANSLATE (keywords into GERMAN): Type 'translate' before the KEYWORD \n"
+						+ "-*---*---*---*---*---*---*---*---*---*----*---*--*---*---*---*---*---*---*---*---*---*----*---*--*---*---*---*---*---*---*---*---*---*----*---*----*----*----*---*---*----*---*----*----*-- \n"
+						+ "Chatbot : How can I help you today? Please input one of the following three options:\n"
+						+ "                                   (-) Android Restoration\n"
+						+ "                                   (-) To View Keywords\n"
+						+ "                                   (-) iPhone Restoration\n");
 
-        //field settings
-        field.setSize(401,20);
-        field.setLocation(1,420);
+		// Adding Action Listener for the Button
+		button.addActionListener((ActionListener) new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 
+				// button press
+				if (e.getSource() == button) {
+					// Getting text from user once BUTTON is pressed
+					String userText = field.getText().toLowerCase();// evrything to lower case
+					area.append("You: " + userText + "\n");// user input is added to text area
+					field.setText("");// reset input box
 
-        //BUTTON SETTINGS
-        button.setSize(500,60); //SIZE
-        button.setLocation(400,420); //LOCATION
+					// control the survey questions after user has asked to exit
+					if (ex) {
 
-        //LABEL SETTING
-        label.setText("RETURN"); //TEXT
-        label.setAlignmentY(button.getAlignmentY()-2);
-        button.add(label); //Assigning the Label to the Bottom
+						area.append("ChatBot: " + survey.SurveyQuestions.get(excount) + "\n");// ask survey question
 
-        //opening statement
-        area.append("Hello! I am Team 30's Virtual Product Assistant!!\n" +
-                "                             TO EXIT PRROGRAM: Type 'exit' or 'done'\n"+
-                "Chatbot : How can I help you today? Please select one of the following two options:\n"+
-                "                             -Android Restoration\n"+
-                "                             -iPhone Restoration\n");
+						System.out.println(excount);
+						System.out.println(survey.SurveyQuestions.size());
+						if (excount != 0)
+							survey.writer(survey.SurveyQuestions.get(excount - 1) + ": " + userText);// writes question
+																										// and answer to
+																										// file
 
-        //Adding Action Listener for the Button
-        button.addActionListener((ActionListener) new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
+						if (excount == (5))
+							System.exit(10);// Closing the program once all questions are answered
+						excount = excount + 1;
+						return;
+					}
+					// find solution to user input
+					String ans = sol.Findsol(userText);
 
+					if (ans.equals("exit")) {// if the user enters exit variable ex is changed
 
-                //button press
-                if(e.getSource()==button) {
-                    //Getting text from user once BUTTON is pressed
-                    String userText = field.getText().toLowerCase();//evrything to lower case
-                    area.append("You: " + userText + "\n");//user input is added to text area
-                    field.setText("");//reset input box
+						ex = true;
+						area.append(
+								"Chatbot: please complete our survey\nplease answer the following questions by entering a score from 1-10\n"
+										+ "1-not at all. 10-totaly agree\npress RETURN to contiue\n");
+					}
 
-                   //control the survey questions after user has asked to exit
-                    if(ex){
+					else
+						reply(ans + ".\nChatbot: What else can I help you with today?");
 
-                        area.append("ChatBot: "+survey.SurveyQuestions.get(excount)+"\n");//ask survey question
+				}
 
-                        System.out.println(excount);
-                        System.out.println(survey.SurveyQuestions.size());
-                        if(excount!=0)
-                        survey.writer(survey.SurveyQuestions.get(excount-1)+": "+userText);//writes question and answer to file
+			}
 
-                        if(excount==(5))
-                            System.exit(10);//Closing the program once all questions are answered
-                        excount=excount+1;
-                        return;
-                    }
-                    //find solution to user input
-                    String ans = sol.Findsol(userText);
+		}); // CLOSING BRACKET for the function that adds the Action Listener to the button
 
-                    if (ans.equals("exit")){//if the user enters exit variable ex is changed
+	}
+	// SELECT-one-of-the-following method
 
-                        ex=true;
-                      area.append("Chatbot: please complete our survey\nplease answer the following questions by entering a score from 1-10\n" +
-                              "1-not at all. 10-totaly agree\npress RETURN to contiue\n");
-                    }
-
-                    else
-                    reply(ans+".\n Chatbot: What else can I help you with");
-
-                }
-
-            }
-
-        }); //CLOSING BRACKET for the function that adds the Action Listener to the button
-
-    }
-    //SELECT-one-of-the-following method
-
-    //The REPLY METHOD
-    public void reply(String input) {area.append("Chatbot: You should try to " + input + "\n");}
-
-  
+	// The REPLY METHOD
+	public void reply(String input) {
+		area.append("Chatbot: " + input + "\n");
+	}
 
 }
--- solution.java
+++ solution.java
@@ -4,21 +4,24 @@ import java.util.*;
 
 public class solution {
     static Device d1;
+
     private static String keyword;
 
     public solution(Device device) {
         d1 = device;
 
 
+
     }
 
     public static String Findsol(String Q) {//takes in Sting
     	ArrayList<String> setResponses = new ArrayList<String>();
-    	setResponses.add(" rephrase your problem. I couldn't understand that");
-    	setResponses.add(" check your spelling. My programming is key-sensitive!");
-    	setResponses.add(" answer in a one to two word manner. Example: data, power off, damage, etc..");
-    	setResponses.add(" get better at spelling because I cannot follow.");
-    	setResponses.add(" go back to school..\n maybe read a book or two, \n eventually learn how to spell, and return with good grammer. ");
+    	setResponses.add("Please rephrase your problem. I couldn't understand that");
+    	setResponses.add("Please check your spelling. My programming is key-sensitive!");
+    	setResponses.add("Please answer in a one to two word manner.\n       (-) Example: translate data, won't power on, define power, etc..");
+    	setResponses.add("Please check out the keywords by typing 'keywords' and try again!");
+    	setResponses.add("To translate a keyword. Type 'translate' followed by keyword.   \n "
+    			+ "      (-) Example: translate power, translate data ");
         Q = Q.toLowerCase(Locale.ROOT);//convert to lowercase
         Boolean ex = false;
         if(Q.equals("exit")||Q.equals("done"))//check if user entered an exit phrase
@@ -45,12 +48,10 @@ public class solution {
         for (int i = 0; i < d1.master.size(); i++) {
             for (int j = 0; j < d1.master.get(i).size(); j++) {
                 if (d1.master.get(i).get(j).equals(keyword)) {
-                    //System.out.println(d1.problems.get(d1.master.get(i)));
                     return d1.problems.get(d1.master.get(i));
                 }
             }
         }
-
         return ("an error has occurred");//if nothing is found an error has occured
     }
 }
